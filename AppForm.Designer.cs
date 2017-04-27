2547e44272dc5eb56f0b24024d8eae28056da19f
Keith Tan <tan230@purdue.edu>
2017-04-27 09:31:54 -0400

    Frontend Application Design
    - Finished GUI for application

--- AppForm.Designer.cs
+++ AppForm.Designer.cs
@@ -28,7 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppForm));
             this.label1 = new System.Windows.Forms.Label();
+            this.lstOut = new System.Windows.Forms.ListBox();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.chkAlergy = new System.Windows.Forms.CheckBox();
+            this.cboFood = new System.Windows.Forms.ComboBox();
+            this.cboTime = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.recLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -36,19 +51,179 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.Size = new System.Drawing.Size(0, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            // 
+            // lstOut
+            // 
+            this.lstOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstOut.FormattingEnabled = true;
+            this.lstOut.ItemHeight = 25;
+            this.lstOut.Location = new System.Drawing.Point(609, 299);
+            this.lstOut.Margin = new System.Windows.Forms.Padding(6);
+            this.lstOut.Name = "lstOut";
+            this.lstOut.Size = new System.Drawing.Size(360, 177);
+            this.lstOut.TabIndex = 21;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(398, 432);
+            this.btnEnd.Margin = new System.Windows.Forms.Padding(6);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(150, 44);
+            this.btnEnd.TabIndex = 20;
+            this.btnEnd.Text = "Exit";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(26, 432);
+            this.btnEnter.Margin = new System.Windows.Forms.Padding(6);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(260, 44);
+            this.btnEnter.TabIndex = 19;
+            this.btnEnter.Text = "Find me a Dining Court";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // chkAlergy
+            // 
+            this.chkAlergy.AutoSize = true;
+            this.chkAlergy.Location = new System.Drawing.Point(299, 335);
+            this.chkAlergy.Margin = new System.Windows.Forms.Padding(6);
+            this.chkAlergy.Name = "chkAlergy";
+            this.chkAlergy.Size = new System.Drawing.Size(82, 29);
+            this.chkAlergy.TabIndex = 18;
+            this.chkAlergy.Text = "Yes";
+            this.chkAlergy.UseVisualStyleBackColor = true;
+            // 
+            // cboFood
+            // 
+            this.cboFood.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFood.FormattingEnabled = true;
+            this.cboFood.Items.AddRange(new object[] {
+            "Chicken",
+            "Beef",
+            "Pork",
+            "Potatoes",
+            "Fish",
+            "Egg",
+            "Cheese",
+            "Pizza",
+            "Burger",
+            "Noodle"});
+            this.cboFood.Location = new System.Drawing.Point(299, 262);
+            this.cboFood.Margin = new System.Windows.Forms.Padding(6);
+            this.cboFood.Name = "cboFood";
+            this.cboFood.Size = new System.Drawing.Size(238, 33);
+            this.cboFood.TabIndex = 17;
+            // 
+            // cboTime
+            // 
+            this.cboTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTime.FormattingEnabled = true;
+            this.cboTime.Items.AddRange(new object[] {
+            "Breakfast",
+            "Lunch",
+            "Dinner"});
+            this.cboTime.Location = new System.Drawing.Point(299, 197);
+            this.cboTime.Margin = new System.Windows.Forms.Padding(6);
+            this.cboTime.Name = "cboTime";
+            this.cboTime.Size = new System.Drawing.Size(238, 33);
+            this.cboTime.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 337);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Food Allergy?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 268);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "What food would you like: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 203);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Select Meal Time: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(608, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(361, 167);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.75F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(0, 34);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(614, 101);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Purdue Dining";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(604, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(310, 25);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Dining Court Recommendation:";
+            // 
+            // recLabel
+            // 
+            this.recLabel.AutoSize = true;
+            this.recLabel.Location = new System.Drawing.Point(604, 262);
+            this.recLabel.Name = "recLabel";
+            this.recLabel.Size = new System.Drawing.Size(0, 25);
+            this.recLabel.TabIndex = 23;
             // 
             // AppForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1600, 1355);
+            this.ClientSize = new System.Drawing.Size(1000, 491);
+            this.Controls.Add(this.recLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lstOut);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.chkAlergy);
+            this.Controls.Add(this.cboFood);
+            this.Controls.Add(this.cboTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
             this.Name = "AppForm";
-            this.Text = "Form1";
+            this.Text = "Purdue Dining Court Reccommender";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +232,19 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstOut;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.CheckBox chkAlergy;
+        private System.Windows.Forms.ComboBox cboFood;
+        private System.Windows.Forms.ComboBox cboTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label recLabel;
     }
 }
 
